f673778a5289d2059f58a5a0c3dbcef346d5c68a
Sankett65 <jadhav.jadhav.sanket@gmail.com>
2023-10-10 18:10:49 +0530

    Using Parameterized Test

--- User_Registration/src/test/java/com/bridgelabz/userregistrationTest.java
+++ User_Registration/src/test/java/com/bridgelabz/userregistrationTest.java
@@ -2,11 +2,39 @@ package com.bridgelabz;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.lang.reflect.Parameter;
+import java.lang.reflect.ParameterizedType;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 
 public class userregistrationTest {
-
     UserRegistration userRegistration = new UserRegistration();
+    @ParameterizedTest
+    @CsvSource({
+            "abc@yahoo.com,true",
+            "abc-100@yahoo.com,false",
+            "abc.100@yahoo.com,false",
+            "abc111@abc.com,false",
+            "abc-100@abc.net,false",
+            "abc.100@abc.com.au,false",
+            "abc@1.com,false" ,
+            "abc@gmail.com.com,false",
+            "abc+100@gmail.com,false"})
 
+
+    void testEmailValidation(String emailAddress, boolean expected) {
+        boolean isValid = userRegistration.email(emailAddress);
+       Assertions.assertTrue(isValid == expected, "Expected: " + expected + ", Actual: " + isValid);
+    }
+
+//
+//
     @Test
     public void toSeeTheFirstNameIsValid(){
         Assertions.assertTrue(userRegistration.Fname("Sanket"));
@@ -34,4 +62,7 @@ public class userregistrationTest {
     }
 
 
+
+//    @ParameterizedTest
+//    @CsvSource({"abc@yahoo.com,true","abc-100@yahoo.com,false","abc.100@yahoo.com,false","abc111@abc.com,false","abc-100@abc.net,false","abc.100@abc.com.au,false","abc@1.com,false" ,"abc@gmail.com.com,false","abc+100@gmail.com,false"})
 }

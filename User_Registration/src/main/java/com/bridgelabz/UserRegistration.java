c0e1166e03fbbfdb9a36f55c7b5e5c45f0289921
Sankett65 <jadhav.jadhav.sanket@gmail.com>
2023-10-10 15:23:55 +0530

    Checking First Name

---
+++ User_Registration/src/main/java/com/bridgelabz/UserRegistration.java
@@ -0,0 +1,19 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UserRegistration {
+
+    public boolean Fname(String name){
+        Pattern pattern = Pattern.compile("^[A-Z]+[a-zA-Z]{2,}[0-9]*$");
+        Matcher matcher = pattern.matcher(name);
+        if (matcher.matches()){
+          return true;
+        }else {
+            return false;
+        }
+    }
+    
+}
